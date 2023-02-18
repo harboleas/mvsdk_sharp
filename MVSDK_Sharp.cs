05876607f5ea9ce37ad418f77e8b8bae81724a3b
Hugo Arboleas <hugo.arboleas@gmail.com>
2023-02-17 23:28:33 -0300

    working

--- MVSDK_Sharp.cs
+++ MVSDK_Sharp.cs
@@ -44,6 +44,48 @@ namespace Camara_MARS
         }
     }
 
+
+    // Definicion de constantes
+    public static class Const
+    {
+        // Error code
+        public const int IMV_OK = 0;
+        public const int IMV_ERROR = -101;
+        public const int IMV_INVALID_HANDLE = -102;
+        public const int IMV_INVALID_PARAM = -103;
+        public const int IMV_INVALID_FRAME_HANDLE = -104;
+        public const int IMV_INVALID_FRAME = -105;
+        public const int IMV_INVALID_RESOURCE = -106;
+        public const int IMV_INVALID_IP = -107;
+        public const int IMV_NO_MEMORY = -108;
+        public const int IMV_INSUFFICIENT_MEMORY = -109;
+        public const int IMV_ERROR_PROPERTY_TYPE = -110;
+        public const int IMV_INVALID_ACCESS = -111;
+        public const int IMV_INVALID_RANGE = -112;
+        public const int IMV_NOT_SUPPORT = -113;
+        //
+        public const int IMV_MAX_DEVICE_ENUM_NUM = 100;
+        public const int IMV_MAX_STRING_LENTH = 256;
+        public const int IMV_MAX_ERROR_LIST_NUM = 128;
+        //
+        public const int IMV_GVSP_PIX_MONO = 0x01000000;
+        public const int IMV_GVSP_PIX_RGB = 0x02000000;
+        public const int IMV_GVSP_PIX_COLOR = 0x02000000;
+        public const uint IMV_GVSP_PIX_CUSTOM = 0x80000000;
+        public const uint IMV_GVSP_PIX_COLOR_MASK = 0xFF000000;
+
+        public const int IMV_GVSP_PIX_OCCUPY1BIT = 0x00010000;
+        public const int IMV_GVSP_PIX_OCCUPY2BIT = 0x00020000;
+        public const int IMV_GVSP_PIX_OCCUPY4BIT = 0x00040000;
+        public const int IMV_GVSP_PIX_OCCUPY8BIT = 0x00080000;
+        public const int IMV_GVSP_PIX_OCCUPY12BIT = 0x000C0000;
+        public const int IMV_GVSP_PIX_OCCUPY16BIT = 0x00100000;
+        public const int IMV_GVSP_PIX_OCCUPY24BIT = 0x00180000;
+        public const int IMV_GVSP_PIX_OCCUPY32BIT = 0x00200000;
+        public const int IMV_GVSP_PIX_OCCUPY36BIT = 0x00240000;
+        public const int IMV_GVSP_PIX_OCCUPY48BIT = 0x00300000;
+    }
+
     // Definicion de estructuras
     public enum IMV_EInterfaceType
     {
@@ -73,22 +115,22 @@ namespace Camara_MARS
     [StructLayout(LayoutKind.Sequential)]
     public struct _string_256 
     {
-        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 256)]
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = Const.IMV_MAX_STRING_LENTH)]
         public string value;
     }
 
     [StructLayout(LayoutKind.Sequential)]
     public struct IMV_GigEInterfaceInfo
     {
-        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 256)]
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = Const.IMV_MAX_STRING_LENTH)]
         public string description;
-        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 256)]
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = Const.IMV_MAX_STRING_LENTH)]
         public string macAddress;
-        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 256)]
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = Const.IMV_MAX_STRING_LENTH)]
         public string ipAddress;
-        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 256)]
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = Const.IMV_MAX_STRING_LENTH)]
         public string subnetMask;
-        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 256)]
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = Const.IMV_MAX_STRING_LENTH)]
         public string defaultGateWay;
         [MarshalAs(UnmanagedType.ByValArray, SizeConst = 5)]
         public _string_256[] chReserved;
@@ -97,17 +139,17 @@ namespace Camara_MARS
     [StructLayout(LayoutKind.Sequential)]
     public struct IMV_UsbInterfaceInfo
     {
-        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 256)]
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = Const.IMV_MAX_STRING_LENTH)]
         public string description;
-        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 256)]
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = Const.IMV_MAX_STRING_LENTH)]
         public string vendorID;
-        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 256)]
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = Const.IMV_MAX_STRING_LENTH)]
         public string deviceID;
-        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 256)]
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = Const.IMV_MAX_STRING_LENTH)]
         public string subsystemID;
-        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 256)]
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = Const.IMV_MAX_STRING_LENTH)]
         public string revision;
-        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 256)]
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = Const.IMV_MAX_STRING_LENTH)]
         public string speed;
         [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
         public _string_256[] chReserved;
@@ -133,17 +175,17 @@ namespace Camara_MARS
 	    [MarshalAs(UnmanagedType.ByValArray, SizeConst = 3)]
         public uint[] nReserved;
 
-        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 256)]
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = Const.IMV_MAX_STRING_LENTH)]
         public string macAddress;
-        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 256)]
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = Const.IMV_MAX_STRING_LENTH)]
         public string ipAddress;
-        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 256)]
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = Const.IMV_MAX_STRING_LENTH)]
         public string subnetMask;
-        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 256)]
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = Const.IMV_MAX_STRING_LENTH)]
         public string defaultGateWay;
-        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 256)]
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = Const.IMV_MAX_STRING_LENTH)]
         public string protocolVersion;
-        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 256)]
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = Const.IMV_MAX_STRING_LENTH)]
         public string ipConfiguration;
 
         [MarshalAs(UnmanagedType.ByValArray, SizeConst = 6)]
@@ -177,27 +219,27 @@ namespace Camara_MARS
         [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
         public uint[] Reserved;
 
-        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 256)]
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = Const.IMV_MAX_STRING_LENTH)]
         public string configurationValid;
-        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 256)]
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = Const.IMV_MAX_STRING_LENTH)]
         public string genCPVersion;
-        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 256)]
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = Const.IMV_MAX_STRING_LENTH)]
         public string u3vVersion;
-        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 256)]
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = Const.IMV_MAX_STRING_LENTH)]
         public string deviceGUID;
-        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 256)]
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = Const.IMV_MAX_STRING_LENTH)]
         public string familyName;
-        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 256)]
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = Const.IMV_MAX_STRING_LENTH)]
         public string u3vSerialNumber;
-        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 256)]
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = Const.IMV_MAX_STRING_LENTH)]
         public string speed;
-        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 256)]
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = Const.IMV_MAX_STRING_LENTH)]
         public string maxPower;
         [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
         public _string_256[] chReserved;
     }
 
-    [StructLayout(LayoutKind.Explicit)]
+    [StructLayout(LayoutKind.Explicit, Size = 3096)]
     public struct _DeviceSpecificInfo
     {
 //        [FieldOffset(0)]
@@ -223,25 +265,25 @@ namespace Camara_MARS
         [MarshalAs(UnmanagedType.ByValArray, SizeConst = 5)]
         public int[] nCameraReserved;
 
-        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 256)]
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = Const.IMV_MAX_STRING_LENTH)]
         public string cameraKey;
 
-        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 256)]
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = Const.IMV_MAX_STRING_LENTH)]
         public string cameraName;
 
-        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 256)]
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = Const.IMV_MAX_STRING_LENTH)]
         public string serialName;
 
-        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 256)]
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = Const.IMV_MAX_STRING_LENTH)]
         public string vendorName;
 
-        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 256)]
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = Const.IMV_MAX_STRING_LENTH)]
         public string modelName;
 
-        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 256)]
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = Const.IMV_MAX_STRING_LENTH)]
         public string manufactureInfo;
         
-        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 256)]
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = Const.IMV_MAX_STRING_LENTH)]
         public string deviceVersion;
 
         [MarshalAs(UnmanagedType.ByValArray, SizeConst = 5)]
@@ -253,7 +295,7 @@ namespace Camara_MARS
 
         [MarshalAs(UnmanagedType.ByValArray, SizeConst = 5)]
         public int[] nInterfaceReserved;
-        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 256)]
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = Const.IMV_MAX_STRING_LENTH)]
         public string InterfaceName;
         [MarshalAs(UnmanagedType.ByValArray, SizeConst = 5)]
         public _string_256[] interfaceReserved;
@@ -262,5 +304,120 @@ namespace Camara_MARS
 
     }
 
+    public enum IMV_EPixelType
+    {
+    	// Undefined pixel type
+    	gvspPixelTypeUndefined = -1,
+
+    	// Mono Format
+    	gvspPixelMono1p = (Const.IMV_GVSP_PIX_MONO | Const.IMV_GVSP_PIX_OCCUPY1BIT | 0x0037),
+    	gvspPixelMono2p = (Const.IMV_GVSP_PIX_MONO | Const.IMV_GVSP_PIX_OCCUPY2BIT | 0x0038),
+    	gvspPixelMono4p = (Const.IMV_GVSP_PIX_MONO | Const.IMV_GVSP_PIX_OCCUPY4BIT | 0x0039),
+    	gvspPixelMono8 = (Const.IMV_GVSP_PIX_MONO | Const.IMV_GVSP_PIX_OCCUPY8BIT | 0x0001),
+    	gvspPixelMono8S = (Const.IMV_GVSP_PIX_MONO | Const.IMV_GVSP_PIX_OCCUPY8BIT | 0x0002),
+    	gvspPixelMono10 = (Const.IMV_GVSP_PIX_MONO | Const.IMV_GVSP_PIX_OCCUPY16BIT | 0x0003),
+    	gvspPixelMono10Packed = (Const.IMV_GVSP_PIX_MONO | Const.IMV_GVSP_PIX_OCCUPY12BIT | 0x0004),
+    	gvspPixelMono12 = (Const.IMV_GVSP_PIX_MONO | Const.IMV_GVSP_PIX_OCCUPY16BIT | 0x0005),
+    	gvspPixelMono12Packed = (Const.IMV_GVSP_PIX_MONO | Const.IMV_GVSP_PIX_OCCUPY12BIT | 0x0006),
+    	gvspPixelMono14 = (Const.IMV_GVSP_PIX_MONO | Const.IMV_GVSP_PIX_OCCUPY16BIT | 0x0025),
+    	gvspPixelMono16 = (Const.IMV_GVSP_PIX_MONO | Const.IMV_GVSP_PIX_OCCUPY16BIT | 0x0007),
+
+    	// Bayer Format
+    	gvspPixelBayGR8 = (Const.IMV_GVSP_PIX_MONO | Const.IMV_GVSP_PIX_OCCUPY8BIT | 0x0008),
+    	gvspPixelBayRG8 = (Const.IMV_GVSP_PIX_MONO | Const.IMV_GVSP_PIX_OCCUPY8BIT | 0x0009),
+    	gvspPixelBayGB8 = (Const.IMV_GVSP_PIX_MONO | Const.IMV_GVSP_PIX_OCCUPY8BIT | 0x000A),
+    	gvspPixelBayBG8 = (Const.IMV_GVSP_PIX_MONO | Const.IMV_GVSP_PIX_OCCUPY8BIT | 0x000B),
+    	gvspPixelBayGR10 = (Const.IMV_GVSP_PIX_MONO | Const.IMV_GVSP_PIX_OCCUPY16BIT | 0x000C),
+    	gvspPixelBayRG10 = (Const.IMV_GVSP_PIX_MONO | Const.IMV_GVSP_PIX_OCCUPY16BIT | 0x000D),
+    	gvspPixelBayGB10 = (Const.IMV_GVSP_PIX_MONO | Const.IMV_GVSP_PIX_OCCUPY16BIT | 0x000E),
+    	gvspPixelBayBG10 = (Const.IMV_GVSP_PIX_MONO | Const.IMV_GVSP_PIX_OCCUPY16BIT | 0x000F),
+    	gvspPixelBayGR12 = (Const.IMV_GVSP_PIX_MONO | Const.IMV_GVSP_PIX_OCCUPY16BIT | 0x0010),
+    	gvspPixelBayRG12 = (Const.IMV_GVSP_PIX_MONO | Const.IMV_GVSP_PIX_OCCUPY16BIT | 0x0011),
+    	gvspPixelBayGB12 = (Const.IMV_GVSP_PIX_MONO | Const.IMV_GVSP_PIX_OCCUPY16BIT | 0x0012),
+    	gvspPixelBayBG12 = (Const.IMV_GVSP_PIX_MONO | Const.IMV_GVSP_PIX_OCCUPY16BIT | 0x0013),
+    	gvspPixelBayGR10Packed = (Const.IMV_GVSP_PIX_MONO | Const.IMV_GVSP_PIX_OCCUPY12BIT | 0x0026),
+    	gvspPixelBayRG10Packed = (Const.IMV_GVSP_PIX_MONO | Const.IMV_GVSP_PIX_OCCUPY12BIT | 0x0027),
+    	gvspPixelBayGB10Packed = (Const.IMV_GVSP_PIX_MONO | Const.IMV_GVSP_PIX_OCCUPY12BIT | 0x0028),
+    	gvspPixelBayBG10Packed = (Const.IMV_GVSP_PIX_MONO | Const.IMV_GVSP_PIX_OCCUPY12BIT | 0x0029),
+    	gvspPixelBayGR12Packed = (Const.IMV_GVSP_PIX_MONO | Const.IMV_GVSP_PIX_OCCUPY12BIT | 0x002A),
+    	gvspPixelBayRG12Packed = (Const.IMV_GVSP_PIX_MONO | Const.IMV_GVSP_PIX_OCCUPY12BIT | 0x002B),
+    	gvspPixelBayGB12Packed = (Const.IMV_GVSP_PIX_MONO | Const.IMV_GVSP_PIX_OCCUPY12BIT | 0x002C),
+    	gvspPixelBayBG12Packed = (Const.IMV_GVSP_PIX_MONO | Const.IMV_GVSP_PIX_OCCUPY12BIT | 0x002D),
+    	gvspPixelBayGR16 = (Const.IMV_GVSP_PIX_MONO | Const.IMV_GVSP_PIX_OCCUPY16BIT | 0x002E),
+    	gvspPixelBayRG16 = (Const.IMV_GVSP_PIX_MONO | Const.IMV_GVSP_PIX_OCCUPY16BIT | 0x002F),
+    	gvspPixelBayGB16 = (Const.IMV_GVSP_PIX_MONO | Const.IMV_GVSP_PIX_OCCUPY16BIT | 0x0030),
+    	gvspPixelBayBG16 = (Const.IMV_GVSP_PIX_MONO | Const.IMV_GVSP_PIX_OCCUPY16BIT | 0x0031),
+
+    	// RGB Format
+    	gvspPixelRGB8 = (Const.IMV_GVSP_PIX_COLOR | Const.IMV_GVSP_PIX_OCCUPY24BIT | 0x0014),
+    	gvspPixelBGR8 = (Const.IMV_GVSP_PIX_COLOR | Const.IMV_GVSP_PIX_OCCUPY24BIT | 0x0015),
+    	gvspPixelRGBA8 = (Const.IMV_GVSP_PIX_COLOR | Const.IMV_GVSP_PIX_OCCUPY32BIT | 0x0016),
+    	gvspPixelBGRA8 = (Const.IMV_GVSP_PIX_COLOR | Const.IMV_GVSP_PIX_OCCUPY32BIT | 0x0017),
+    	gvspPixelRGB10 = (Const.IMV_GVSP_PIX_COLOR | Const.IMV_GVSP_PIX_OCCUPY48BIT | 0x0018),
+    	gvspPixelBGR10 = (Const.IMV_GVSP_PIX_COLOR | Const.IMV_GVSP_PIX_OCCUPY48BIT | 0x0019),
+    	gvspPixelRGB12 = (Const.IMV_GVSP_PIX_COLOR | Const.IMV_GVSP_PIX_OCCUPY48BIT | 0x001A),
+    	gvspPixelBGR12 = (Const.IMV_GVSP_PIX_COLOR | Const.IMV_GVSP_PIX_OCCUPY48BIT | 0x001B),
+    	gvspPixelRGB16 = (Const.IMV_GVSP_PIX_COLOR | Const.IMV_GVSP_PIX_OCCUPY48BIT | 0x0033),
+    	gvspPixelRGB10V1Packed = (Const.IMV_GVSP_PIX_COLOR | Const.IMV_GVSP_PIX_OCCUPY32BIT | 0x001C),
+    	gvspPixelRGB10P32 = (Const.IMV_GVSP_PIX_COLOR | Const.IMV_GVSP_PIX_OCCUPY32BIT | 0x001D),
+    	gvspPixelRGB12V1Packed = (Const.IMV_GVSP_PIX_COLOR | Const.IMV_GVSP_PIX_OCCUPY36BIT | 0X0034),
+    	gvspPixelRGB565P = (Const.IMV_GVSP_PIX_COLOR | Const.IMV_GVSP_PIX_OCCUPY16BIT | 0x0035),
+    	gvspPixelBGR565P = (Const.IMV_GVSP_PIX_COLOR | Const.IMV_GVSP_PIX_OCCUPY16BIT | 0X0036),
+
+    	// YVR Format
+    	gvspPixelYUV411_8_UYYVYY = (Const.IMV_GVSP_PIX_COLOR | Const.IMV_GVSP_PIX_OCCUPY12BIT | 0x001E),
+    	gvspPixelYUV422_8_UYVY = (Const.IMV_GVSP_PIX_COLOR | Const.IMV_GVSP_PIX_OCCUPY16BIT | 0x001F),
+    	gvspPixelYUV422_8 = (Const.IMV_GVSP_PIX_COLOR | Const.IMV_GVSP_PIX_OCCUPY16BIT | 0x0032),
+    	gvspPixelYUV8_UYV = (Const.IMV_GVSP_PIX_COLOR | Const.IMV_GVSP_PIX_OCCUPY24BIT | 0x0020),
+    	gvspPixelYCbCr8CbYCr = (Const.IMV_GVSP_PIX_COLOR | Const.IMV_GVSP_PIX_OCCUPY24BIT | 0x003A),
+    	gvspPixelYCbCr422_8 = (Const.IMV_GVSP_PIX_COLOR | Const.IMV_GVSP_PIX_OCCUPY16BIT | 0x003B),
+    	gvspPixelYCbCr422_8_CbYCrY = (Const.IMV_GVSP_PIX_COLOR | Const.IMV_GVSP_PIX_OCCUPY16BIT | 0x0043),
+    	gvspPixelYCbCr411_8_CbYYCrYY = (Const.IMV_GVSP_PIX_COLOR | Const.IMV_GVSP_PIX_OCCUPY12BIT | 0x003C),
+    	gvspPixelYCbCr601_8_CbYCr = (Const.IMV_GVSP_PIX_COLOR | Const.IMV_GVSP_PIX_OCCUPY24BIT | 0x003D),
+    	gvspPixelYCbCr601_422_8 = (Const.IMV_GVSP_PIX_COLOR | Const.IMV_GVSP_PIX_OCCUPY16BIT | 0x003E),
+    	gvspPixelYCbCr601_422_8_CbYCrY = (Const.IMV_GVSP_PIX_COLOR | Const.IMV_GVSP_PIX_OCCUPY16BIT | 0x0044),
+    	gvspPixelYCbCr601_411_8_CbYYCrYY = (Const.IMV_GVSP_PIX_COLOR | Const.IMV_GVSP_PIX_OCCUPY12BIT | 0x003F),
+    	gvspPixelYCbCr709_8_CbYCr = (Const.IMV_GVSP_PIX_COLOR | Const.IMV_GVSP_PIX_OCCUPY24BIT | 0x0040),
+    	gvspPixelYCbCr709_422_8 = (Const.IMV_GVSP_PIX_COLOR | Const.IMV_GVSP_PIX_OCCUPY16BIT | 0x0041),
+    	gvspPixelYCbCr709_422_8_CbYCrY = (Const.IMV_GVSP_PIX_COLOR | Const.IMV_GVSP_PIX_OCCUPY16BIT | 0x0045),
+    	gvspPixelYCbCr709_411_8_CbYYCrYY = (Const.IMV_GVSP_PIX_COLOR | Const.IMV_GVSP_PIX_OCCUPY12BIT | 0x0042),
+
+    	// RGB Planar
+    	gvspPixelRGB8Planar = (Const.IMV_GVSP_PIX_COLOR | Const.IMV_GVSP_PIX_OCCUPY24BIT | 0x0021),
+    	gvspPixelRGB10Planar = (Const.IMV_GVSP_PIX_COLOR | Const.IMV_GVSP_PIX_OCCUPY48BIT | 0x0022),
+    	gvspPixelRGB12Planar = (Const.IMV_GVSP_PIX_COLOR | Const.IMV_GVSP_PIX_OCCUPY48BIT | 0x0023),
+    	gvspPixelRGB16Planar = (Const.IMV_GVSP_PIX_COLOR | Const.IMV_GVSP_PIX_OCCUPY48BIT | 0x0024),
+
+    	//BayerRG10p和BayerRG12p格式，针对特定项目临时添加,请不要使用
+    	//BayerRG10p and BayerRG12p, currently used for specific project, please do not use them
+    	gvspPixelBayRG10p = 0x010A0058,
+    	gvspPixelBayRG12p = 0x010c0059,
+
+    	//mono1c格式，自定义格式
+    	//mono1c, customized image format, used for binary output
+    	gvspPixelMono1c = 0x012000FF,
+
+    	//mono1e格式，自定义格式，用来显示连通域
+    	//mono1e, customized image format, used for displaying connected domain
+    	gvspPixelMono1e = 0x01080FFF
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct IMV_FrameInfo
+    {
+    	public UInt64 blockId;				
+    	public uint	status;		
+    	public uint	width;		
+    	public uint	height;
+    	public uint	size;	
+    	IMV_EPixelType pixelFormat;	
+    	public UInt64 timeStamp;				
+    	public uint chunkCount;
+    	public uint	paddingX;	
+    	public uint	paddingY;
+    	public uint	recvFrameTime;			
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 19)]
+    	public uint[] nReserved;		
+    }
 
 }
